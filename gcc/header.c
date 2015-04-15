750982cfb26260bfe4bed4d70d06f40a69f321b4
stonedmind <stonrmind@gmail.com>
2015-04-15 16:34:11 +0530

    added http header builder

--- gcc/header.c
+++ gcc/header.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 #define HEAD 	0
 #define GET  	1
@@ -12,87 +13,111 @@
 
 char * header;
 
-void build_get(char * host, char * path, char * str)
-{
-	memcpy(str, "GET ",sizeof("GET "));
-	strcat(str, "/");
-	strcat(str, path);
-	strcat(str, " HTTP/1.1\r\n");
-	strcat(str, "Host: ");
-	strcat(str, host);
-	strcat(str, "\r\n");
-	strcat(str, "Content-Type: application/json\r\n");
-	strcat(str, "Accept: application/json\r\n\r\n");
+void build_header(	uint8_t method,
+					char * 	host,
+					char * 	path,
+					char * 	dat,
+					char * 	buff
+					){
+
+	char len_buff[20];
+	// Add first header
+	switch(method)
+	{
+		case HEAD: 		memcpy(buff, "HEAD ",sizeof("HEAD "));
+						break;
+		case POST: 		memcpy(buff, "POST ",sizeof("POST "));
+						break;
+		case GET: 		memcpy(buff, "GET ",sizeof("GET "));
+						break;
+		case PUT: 		memcpy(buff, "PUT ",sizeof("PUT "));
+						break;
+		case DELETE: 	memcpy(buff, "DELETE ",sizeof("DELETE "));
+						break;
+		default:		memcpy(buff, "HEAD ",sizeof("HEAD "));
+						break;
+	}
+
+	// Add path
+	strcat(buff, "/");
+	strcat(buff, path);	
+
+	// Add HTTP verion info
+	strcat(buff, " HTTP/1.1\r\n");
+
+	// Add Host info
+	strcat(buff, "Host: ");
+	strcat(buff, host);
+	strcat(buff, "\r\n");
+
+	// Add Content-Type and Accept
+	strcat(buff, "Content-Type: application/json\r\n");
+	strcat(buff, "Accept: application/json\r\n");
+
+	if(method == HEAD || method == DELETE || method == GET)
+	{
+		strcat(buff, "\r\n");
+		return;
+	}
+	else if(method == PUT || method == POST)
+	{
+		sprintf(len_buff, "%d", strlen(dat));
+		strcat(buff, "Content-Length: ");
+		strcat(buff, len_buff);
+		strcat(buff, "\r\n\r\n");
+		strcat(buff, dat);
+		strcat(buff, "\r\n\r\n");		
+	}
 }
 
-void build_post(char * host, char * path, char * dat, char * str)
+void testHeader(void)
 {
-	char buf[20];
-	sprintf(buf, "%d", strlen(dat));
-	memcpy(str, "POST ",sizeof("POST "));
-	strcat(str, "/");
-	strcat(str, path);
-	strcat(str, " HTTP/1.1\r\n");
-	strcat(str, "Host: ");
-	strcat(str, host);
-	strcat(str, "\r\n");
-	strcat(str, "Content-Type: application/json\r\n");
-	strcat(str, "Accept: application/json\r\n");
-	strcat(str, "Content-Length: ");
-	strcat(str, buf);
-	strcat(str, "\r\n\r\n");
-	strcat(str, dat);
-	strcat(str, "\r\n\r\n");
-}
+	build_header(	HEAD,
+					"google.com",
+					"search",
+					NULL,
+					header
+					);
+	printf("HEAD\n%s\n",header);
 
-void build_put(char * host, char * path, char * dat, char * str)
-{
-	char buf[20];
-	sprintf(buf, "%d", strlen(dat));
-	memcpy(str, "PUT ",sizeof("POST "));
-	strcat(str, "/");
-	strcat(str, path);
-	strcat(str, " HTTP/1.1\r\n");
-	strcat(str, "Host: ");
-	strcat(str, host);
-	strcat(str, "\r\n");
-	strcat(str, "Content-Type: application/json\r\n");
-	strcat(str, "Accept: application/json\r\n");
-	strcat(str, "Content-Length: ");
-	strcat(str, buf);
-	strcat(str, "\r\n\r\n");
-	strcat(str, dat);
-	strcat(str, "\r\n\r\n");
-}
+	build_header(	GET,
+					"google.com",
+					"search?q=test",
+					NULL,
+					header
+					);
+	printf("GET\n%s\n",header);
 
-void build_delete(char * host, char * path, char * str)
-{
-	memcpy(str, "DELETE ",sizeof("DELETE "));
-	strcat(str, "/");
-	strcat(str, path);
-	strcat(str, " HTTP/1.1\r\n");
-	strcat(str, "Host: ");
-	strcat(str, host);
-	strcat(str, "\r\n");
-	strcat(str, "Content-Type: application/json\r\n");
-	strcat(str, "Accept: application/json\r\n\r\n");
-}
+	build_header(	POST,
+					"google.com",
+					"search",
+					"{\"info\":\"1,100\"}",
+					header
+					);
+	printf("POST\n%s\n",header);
 
-void build_header(	char * host,
-					char * path,
-					char * dat,
-					)
+	build_header(	PUT,
+					"google.com",
+					"search",
+					"{\"info\":\"1,100\"}",
+					header
+					);
+	printf("PUT\n%s\n",header);
+
+	build_header(	DELETE,
+					"google.com",
+					"search",
+					NULL,
+					header
+					);
+	printf("DELETE\n%s\n",header);
+
+}
 
 
 int main(void)
 {
 	header = (char *) malloc(512);
-	build_get("google.com","weight",header);
-	printf("GET Header\n%s\n", header);
-	build_post("google.com","weight","{\"info\":\"1,100\"}",header);
-	printf("POST Header\n%s\n", header);
-	build_put("google.com","weight/1","{\"info\":\"1,100\"}",header);
-	printf("PUT Header\n%s\n", header);
-	build_delete("google.com","weight/1",header);
-	printf("DELETE Header\n%s\n", header);
+	testHeader();
+	return 0;
 }
